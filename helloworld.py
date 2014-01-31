c403e67f01724e8929f0d1b2779b8c7d5cd8dce5
prithvirajpandian <prithvi.pandian@berkeley.edu>
2014-01-31 05:47:29 -0800

    Created app

---
+++ helloworld.py
@@ -0,0 +1,26 @@
+from bottle import get, post, request, route, run
+
+@route('/hello')
+def hello():
+    return "Hello Danny!"
+
+@get('/login') # or @route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if (username=='Danny Radding' and password=='RedSolo1'):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+run(host='localhost', port=8080, debug=True)
