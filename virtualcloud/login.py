72d243510026b56d71c45756f87e35adf06fdd89
prithvirajpandian <prithvi.pandian@berkeley.edu>
2014-03-05 04:35:45 -0800

    cleaned up login.py

--- virtualcloud/login.py
+++ virtualcloud/login.py
@@ -5,30 +5,27 @@ import gd_ops
 import os
 
 def login():
-    data = ""
-
     try:
-        with open(".virtualcloud", "r+") as json_data:
+        with open(".virtualcloud", "w+") as json_data:
             data = json.load(json_data)
     except IOError:
-        print "Creating new settings file"
-        data = {'dropbox' : [], 'gdrive' : []}
-        continue
+        print "Could not create new settings file"
 
+    if not data:
+        data = {'dropbox' : [], 'gdrive' : []}
+        
     while True:
-        cmd = raw_input("To add a service please enter 'dropbox' or 'gdrive'. To go back enter 'back': ")
-        cmd.lower()
+        cmd = raw_input("To add a service please enter 'dropbox' or 'gdrive'. When done, enter 'done': ").lower()
         if cmd == 'dropbox':
             db = db_ops.db(-1)
             data['dropbox'].append(ndb.AT)          
         if cmd == 'gdrive':
             gd = gd_ops.gd(-1)
             data['gdrive'].append(ngd.credentials)        
-        if cmd == 'back':
+        if cmd == 'done':
             break
 
     try:
-        os.remove(".virtualcloud")
         with open(".virtualcloud", "w+") as json_file:
             json.dump(data,json_file)
     except IOError:
