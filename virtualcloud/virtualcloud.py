72051d03376a5b7c4eeff145594aa33625acc964
prithvirajpandian <prithvi.pandian@berkeley.edu>
2014-03-04 21:13:06 -0800

    changed virtualcloud.py for dual-mode access, shell or cli

--- virtualcloud/virtualcloud.py
+++ virtualcloud/virtualcloud.py
@@ -1,1 +1,17 @@
-pass
+#!/usr/bin/python
+import sys, getopt, virtualcloud
+
+
+
+if len(sys.argv) == 1:
+    VirtualCloudShell().cmdloop()
+
+options = {'setup' : virtualcloud.setup,
+           'login' : virtualcloud.login,
+           'upload' : virtualcloud.upload,
+           'download' : virtualcloud.download
+           }
+           
+options[argv[1]](argv[2:])
+
+
