8adf82c03500467da968a9961ba3ead940a26b75
prithvirajpandian <prithvi.pandian@berkeley.edu>
2014-03-04 23:05:54 -0800

    more progress on upload

--- virtualcloud/upload.py
+++ virtualcloud/upload.py
@@ -19,20 +19,25 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv,"hdgf:s:",["filename=","sizechunk="])
     except getopt.GetoptError:
-        print 'test.py -i <inputfile> -o <outputfile>'
+        print 'upload -f <filepath> -s <chunksize> -d [for dropbox] -g [for gdrive]'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-           print 'test.py -i <inputfile> -o <outputfile>'
+           print 'upload -f <filepath> -s <chunksize> -d [for dropbox] -g [for gdrive]'
            sys.exit()
         elif opt == '-d':
+            dropbox = true
         elif opt == '-g':
+            gdrive = true
         elif opt in ("-f", "--file"):
             filename = arg
         elif opt in ("-s", "--sizechunk"):
             chunksize = int(opt)
 
     prefix = hashlib.sha224(filename).hexdigest()
+
+
+
     split_bash = "split -b" + chunksize + " " + inputfile + " " + prefix
     
     with tempfile.NamedTemporaryFile() as scriptfile:
@@ -42,7 +47,26 @@ def main(argv):
     print 'Input file is "', inputfile
     print 'Output file is "', outputfile
 
+def split_file(file, prefix, max_size, buffer=1024):
+    if dropbox:
+        db = db_ops.db()
+        
+    with open (file, 'r+b') as src:
+        suffix = 0
+        while True:
+            with tempfile.TemporaryFile() as target:
+                written = 0
+                while written <= max_size
+                    data = src.read(buffer)
+                    if data:
+                        target.write(data)
+                        written += buffer
+                    else:
+                        return suffix
+                suffix +=1
 
+                
+open(prefix + '.%s' % suffix, 'w+b'
     
 if __name__ == "__main__":
    main(sys.argv[1:])
