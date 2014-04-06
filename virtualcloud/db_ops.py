33e680b22e93674130ede5baa8ad911b211792d6
Alex Ottoboni <alexottoboni@gmail.com>
2014-04-06 00:20:12 -0700

    Cleaned up Dropbox Upload and added comments

--- virtualcloud/db_ops.py
+++ virtualcloud/db_ops.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # Include the Dropbox SDK
 import dropbox
+import os
 
 # Get your app key and secret from the Dropbox developer website
 app_key = 'pp92jonnw43vmz8'
@@ -8,18 +9,18 @@ app_secret = 'il7w6zl9blvejok'
 flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 
 class db(object):
-    AT = ''
+    AuthToken = ''
     client = 0
     
     def __init__(self, token):
-        self.AT = token
+        self.AuthToken = token
         if token == -1:
             self.db_login()
         else:
-            self.client = dropbox.client.DropboxClient(self.AT)
+            self.client = dropbox.client.DropboxClient(self.AuthToken)
 
     def db_login(self):
-        # Have the user sign in and authorize this1 token
+        # Have the user sign in and authorize this token
         authorize_url = flow.start()
         print '1. Go to: ' + authorize_url
         print '2. Click "Allow" (you might have to log in first)'
@@ -28,12 +29,12 @@ class db(object):
 
         # This will fail if the user enters an invalid authorization code
         access_token, user_id = flow.finish(code)
-        self.AT = access_token
+        self.AuthToken = access_token
         self.client = dropbox.client.DropboxClient(access_token)
         print 'linked account: ', self.client.account_info()
 
-    def db_upload(self, path_db, user_file):
-        response = self.client.put_file('/' + path_db, user_file)
+    def db_upload(self, path_db):
+        response = self.client.put_file('/virtualcloud' + os.path.basename(path_db), path_db)
     
     def db_download(self, path_db):
         f, metadata = self.client.get_file_and_metadata('/'+ path_db)
